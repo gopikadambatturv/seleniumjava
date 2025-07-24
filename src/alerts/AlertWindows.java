92827cdd305d1c1db8122a35356b58fcbb50a943
gopikadambatturv <gopikv36@gmail.com>
2025-07-24 11:02:30 +0530

    Alert Windows Class Updated

--- src/alerts/AlertWindows.java
+++ src/alerts/AlertWindows.java
@@ -3,15 +3,18 @@ package alerts;
 import org.testng.annotations.Test;
 import org.testng.annotations.BeforeMethod;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.AfterMethod;
 
 public class AlertWindows {
 	WebDriver driver;
   @Test
   public void Techlearn() {
+	  driver.get("https://www.techlearn.in");
   }
   @BeforeMethod
   public void beforeMethod() {
+	  driver = new ChromeDriver();
   }
 
   @AfterMethod
