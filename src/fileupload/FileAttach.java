f315e1272951331aec53eb396b541692d61f8dba
gopikadambatturv <gopikv36@gmail.com>
2025-07-24 11:12:44 +0530

    Update FileAttach.java

--- src/fileupload/FileAttach.java
+++ src/fileupload/FileAttach.java
@@ -11,7 +11,7 @@ public class FileAttach {
   @Test
   public void uploadfile() throws InterruptedException {
 	  driver.get("https://www.w3schools.com/howto/howto_html_file_upload_button.asp");
-	  Thread.sleep(3000);
+	  Thread.sleep(5000);
 	  driver.findElement(By.xpath("//*[@id=\"myFile\"]")).sendKeys("D:\\Gopi doc\\Intermediate.jpg");
   }
   @BeforeTest
